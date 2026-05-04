b9090de22e7b44786ec584356396a8017f48ecfc
Jerome1b <jedrekkuterasinski@gmail.com>
2026-05-04 21:41:42 +0200

    Te testy

--- src/Test_Localization.cpp
+++ src/Test_Localization.cpp
@@ -1,6 +1,23 @@
 #include "Localization.cpp"
 #include <gtest/gtest.h>
 
+
+TEST(LocationTest, CheckCoords){
+	location loc(51.05,19.94);
+	
+	EXPECT_DOUBLE_EQ(loc.getX(), 51.05);
+    	EXPECT_DOUBLE_EQ(loc.getY(), 19.94);
+}
+
+TEST(LocationTest, CheckUrl){
+	location loc(1.5, 2.5);
+	
+	std::string url = loc.genUrl;
+	std::string exp = "https://www.google.com/maps/search/?api=1&query=1.500000,2.500000";
+	
+	EXPECT_EQ(url,exp);	
+}
+
 TEST(LocationTest, MapaPrintsCorrectCoordinates) {
     location loc(10.5, 20.25);
 
