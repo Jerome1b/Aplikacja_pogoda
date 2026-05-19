5441a6a7df42b7823fb260a7eef7b68e94a14f28
Nazzz-Py <j.kotowicz213@gmail.com>
2026-05-19 16:46:29 +0200

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "app.hpp"
+using namespace std;
+
+int main(){
+	string miasto;
+	cout << "Podaj miasto: ";
+	cin >> miasto;
+	CURL_Klient klient;
+	Extract_data a(miasto, klient);
+	Location p(a.x,a.y);
+	Wind_inf q(a.v,a.deg);
+	p.mapa();
+	cout << endl << q.kat()<<endl;;
+	return 0; 
+}
